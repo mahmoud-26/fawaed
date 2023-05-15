50e2146e5c5b15e997d143bcdac7f3b2d4d3b31e
mnaggar2006@gmail.com <mnaggar2006@gmail.com>
2023-05-15 20:13:10 +0300

    add amin files

---
+++ script.js
@@ -0,0 +1,88 @@
+function addQuote() {
+  var quote = document.getElementById("quoteInput").value;
+  var author = document.getElementById("authorInput").value;
+  var quoteList = document.getElementById("quoteList");
+
+  if (!quote || !author) {
+    alert("Please enter a quote and author.");
+    return;
+  }
+
+  // Get the existing quotes from local storage or create an empty array
+  var savedQuotes = JSON.parse(localStorage.getItem("quotes")) || [];
+
+  // Add the new quote to the array and save it back to local storage
+  savedQuotes.push({
+    quote: quote,
+    author: author
+  });
+  localStorage.setItem("quotes", JSON.stringify(savedQuotes));
+
+  // Create a new div to display the quote and a delete button
+  var newQuoteItem = document.createElement("div");
+  newQuoteItem.classList.add("quote-card");
+
+  var quoteText = document.createElement("div");
+  quoteText.innerText = quote;
+  quoteText.classList.add("quote-text");
+  newQuoteItem.appendChild(quoteText);
+
+  var authorText = document.createElement("div");
+  authorText.innerText = "- " + author;
+  authorText.classList.add("author-text");
+  newQuoteItem.appendChild(authorText);
+
+  var deleteButton = document.createElement("div");
+  deleteButton.innerHTML = '<ion-icon name="trash"></ion-icon>';
+  deleteButton.classList.add("delete-button");
+  deleteButton.onclick = createDeleteHandler(savedQuotes[savedQuotes.length - 1], newQuoteItem);
+  newQuoteItem.appendChild(deleteButton);
+
+  quoteList.appendChild(newQuoteItem);
+
+  // Reset the input fields
+  document.getElementById("quoteInput").value = "";
+  document.getElementById("authorInput").value = "";
+}
+
+// Load any saved quotes when the page loads
+window.onload = function() {
+  var savedQuotes = JSON.parse(localStorage.getItem("quotes")) || [];
+  var quoteList = document.getElementById("quoteList");
+
+  for (var i = 0; i < savedQuotes.length; i++) {
+    var newQuoteItem = document.createElement("div");
+    newQuoteItem.classList.add("quote-card");
+
+    var quoteText = document.createElement("div");
+    quoteText.innerText = savedQuotes[i].quote;
+    quoteText.classList.add("quote-text");
+    newQuoteItem.appendChild(quoteText);
+
+    var authorText = document.createElement("div");
+    authorText.innerText = "- " + savedQuotes[i].author;
+    authorText.classList.add("author-text");
+    newQuoteItem.appendChild(authorText);
+
+    var deleteButton = document.createElement("div");
+    deleteButton.innerHTML = '<ion-icon name="trash"></ion-icon>';
+    deleteButton.classList.add("delete-button");
+    deleteButton.onclick = createDeleteHandler(savedQuotes[i], newQuoteItem);
+    newQuoteItem.appendChild(deleteButton);
+
+    quoteList.appendChild(newQuoteItem);
+  }
+}
+
+// Helper function to create a delete handler function for each quote
+function createDeleteHandler(quote, quoteItem) {
+  return function() {
+    // Remove the quote from the savedQuotes array and update local storage
+    var savedQuotes = JSON.parse(localStorage.getItem("quotes")) || [];
+    savedQuotes.splice(savedQuotes.indexOf(quote), 1);
+    localStorage.setItem("quotes", JSON.stringify(savedQuotes));
+
+    // Remove the quote item from the quote list
+    quoteItem.remove();
+  };
+}
